4483ca3156800c6c4a55874fd8abe305de073f6a
kale <kale@musicane.com>
2009-07-24 23:52:43 -0700

    moving the site.css again

--- vendor/extensions/site/site_extension.rb
+++ vendor/extensions/site/site_extension.rb
@@ -14,7 +14,7 @@ class SiteExtension < Spree::Extension
   
   def activate 
     AppConfiguration.class_eval do 
-      preference :stylesheets, :string, :default => 'compiled/screen,stylesheets/site' 
+      preference :stylesheets, :string, :default => 'compiled/screen,site' 
     end 
   end 
 end
